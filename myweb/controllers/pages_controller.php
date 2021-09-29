7bf5b28b743c9cfc2572f0dade29d989b0e69a0d
zinxia <91516872+zinxia@users.noreply.github.com>
2021-09-29 12:16:30 +0700

    Add files via upload

---
+++ myweb/controllers/pages_controller.php
@@ -0,0 +1,9 @@
+<?php
+class Pages Controller
+{	public function home()
+	{	require_once('views/pages/home.php');	}
+	public function error() 
+	{	require_once("views/pages/error.php");	}
+
+}
+?>
---
+++ myweb/index.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_GET['controller']) && isset($_GET['action'])) 
+{   $controller = $_GET['controller'];
+    $action = $_GET['action');
+
+}else
+
+{   $controller= 'pages'; 
+    $action = 'home';
+
+} ?>
+<html>
+<head></head>
+<body>
+    <?php echo "controller= ".Scontroller.", action="$action.; ?> 
+    <br>[<a href="">Home </a>] <br>
+    <?php require_once("routes.php"); ?>
+</body>
+</html>
