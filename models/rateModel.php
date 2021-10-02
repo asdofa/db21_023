2d559d983ca10b6b91943c4a68fb9bcfd246f39f
asdofa <91516747+asdofa@users.noreply.github.com>
2021-10-02 22:21:55 +0700

    Update rateModel.php

--- models/rateModel.php
+++ models/rateModel.php
@@ -1,29 +1,32 @@
 <?php
-    class Customer{
-        public $id;
-        public $name;
-        public $address;
-        public $phone;
-        public function __construct($id,$name,$address,$phone){
-            $this->id=$id;
-            $this->name=$name;
-            $this->address=$address;
-            $this->phone=$phone;
+    class Rate{
+        public $PROD_ID;
+        public $PROD_QTY;
+        public $PROD_Price;
+        public $PROD_SCPrice;
+        public $QTY_Rate;
+        public function __construct($PROD_ID,$PROD_QTY,$PROD_Price,$PROD_SCPrice,$QTY_Rate){
+            $this->PROD_ID = $PROD_ID;
+            $this->PROD_QTY = $PROD_QTY;
+            $this->PROD_Price = $PROD_Price;
+            $this->PROD_SCPrice = $PROD_SCPrice;
+            $this->QTY_Rate = $QTY_Rate;
         }
         public static function getAll(){
-            $customerList=[];
+            $rateList=[];
             require("connection_connect.php");
-            $sql="SELECT * FROM CUSTOMER";
+            $sql="SELECT * FROM RATE";
             $result=$conn->query($sql);
             while($my_row = $result->fetch_assoc()){
-                $id=$my_row["C_ID"];
-                $name=$my_row["C_Name"];
-                $address=$my_row["C_Address"];
-                $phone=$my_row["C_Phone"];
-                $customerList[]=new CUSTOMER($id,$name,$address,$phone);
+                $PROD_ID=$my_row["PROD_ID"];
+                $PROD_QTY=$my_row["PROD_QTY"];
+                $PROD_Price=$my_row["PROD_Price"];
+                $PROD_SCPrice=$my_row["PROD_SCPrice"];
+                $QTY_Rate=$my_row["QTY_Rate"];
+                $rateList[]=new RATE($PROD_ID,$PROD_QTY,$PROD_Price,$PROD_SCPrice,$QTY_Rate);
             }
             require("connection_close.php");
-            return $customerList;
+            return $rateList;
         }
     }
 ?>
