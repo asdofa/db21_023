632bdb16185af588e7dfe500a0bbb74ff3763d5f
asdofa <91516747+asdofa@users.noreply.github.com>
2021-10-02 21:38:48 +0700

    Add files via upload

---
+++ connection_close.php
@@ -0,0 +1,9 @@
+<?php
+    $severname = "localhost";
+    $username = "db21_023";
+    $password ="db21_023";
+    $dbname = "db21_023";
+
+    $conn = mysqli_connect($severname,$username,$password);
+    $conn->close();
+?>
