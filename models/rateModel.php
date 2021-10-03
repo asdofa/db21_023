75a0921897ea9eb8bae4d27bf53a09fa9b3571f0
asdofa <91516747+asdofa@users.noreply.github.com>
2021-10-03 11:22:40 +0700

    Update rateModel.php

--- models/rateModel.php
+++ models/rateModel.php
@@ -1,23 +1,27 @@
 <?php
-    class Rate{
+    class Rate
+    {
         public $PROD_ID;
         public $PROD_QTY;
         public $PROD_Price;
         public $PROD_SCPrice;
         public $QTY_Rate;
-        public function __construct($PROD_ID,$PROD_QTY,$PROD_Price,$PROD_SCPrice,$QTY_Rate){
+        public function __construct($PROD_ID,$PROD_QTY,$PROD_Price,$PROD_SCPrice,$QTY_Rate)
+        {
             $this->PROD_ID = $PROD_ID;
             $this->PROD_QTY = $PROD_QTY;
             $this->PROD_Price = $PROD_Price;
             $this->PROD_SCPrice = $PROD_SCPrice;
             $this->QTY_Rate = $QTY_Rate;
         }
-        public static function getAll(){
+        public static function getAll()
+        {
             $rateList=[];
             require("connection_connect.php");
             $sql="SELECT * FROM RATE";
             $result=$conn->query($sql);
-            while($my_row = $result->fetch_assoc()){
+            while($my_row = $result->fetch_assoc())
+            {
                 $PROD_ID=$my_row["PROD_ID"];
                 $PROD_QTY=$my_row["PROD_QTY"];
                 $PROD_Price=$my_row["PROD_Price"];
@@ -28,5 +32,6 @@
             require("connection_close.php");
             return $rateList;
         }
+        
     }
 ?>
