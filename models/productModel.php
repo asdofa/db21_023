f6401991d80f6f6e8b0b37b2d0d58fd1fe897860
zinxia <tananan939@gmail.com>
2021-10-03 23:42:03 +0700

    update

--- models/productModel.php
+++ models/productModel.php
@@ -1,16 +1,18 @@
 <?php
-    class Rate
+    class Product
     {
         public $PROD_ID;
         public $PROD_Name;
+        public $PROD_QTY;
         public $PROD_Category;
         public $PROD_Type;
         public $PROD_Minimum;
         public $PROD_Detail;
-        public function __construct($PROD_ID,$PROD_PROD_NameQTY,$PROD_Category,$PROD_Type,$PROD_Minimum,$PROD_Detail)
+        public function __construct($PROD_ID,$PROD_QTY,$PROD_Name,$PROD_Category,$PROD_Type,$PROD_Minimum,$PROD_Detail)
         {
             $this->PROD_ID = $PROD_ID;
             $this->PROD_Name = $PROD_Name;
+            $this->$PROD_QTY = $PROD_QTY;
             $this->PROD_Category = $PROD_Category;
             $this->PROD_Type = $PROD_Type;
             $this->PROD_Minimum = $PROD_Minimum;
@@ -18,7 +20,7 @@
         }
         public static function getAll()
         {
-            $rateList=[];
+            $productList=[];
             require("connection_connect.php");
             $sql="SELECT * FROM PRODUCT";
             $result=$conn->query($sql);
@@ -26,15 +28,16 @@
             {
                 $PROD_ID=$my_row["PROD_ID"];
                 $PROD_Name=$my_row["PROD_Name"];
+                $PROD_QTY = $my_row["PROD_QTY"];
                 $PROD_Category=$my_row["PROD_Category"];
                 $PROD_Type=$my_row["PROD_Type"];
                 $PROD_Minimum=$my_row["PROD_Minimum"];
                 $PROD_Detail=$my_row["PROD_Detail"];
-                $rateList[]=new RATE($PROD_ID,$PROD_PROD_NameQTY,$PROD_Category,$PROD_Type,$PROD_Minimum,$PROD_Detail);
+                $productList[]=new Product($PROD_ID,$PROD_QTY,$PROD_Name,$PROD_Category,$PROD_Type,$PROD_Minimum,$PROD_Detail);
             }
             require("connection_close.php");
 
-            return $rateList;
+            return $productList;
         }
     }
 ?>
---
+++ views/QuotationDetail/newQuotationDetail.php
@@ -0,0 +1,22 @@
+<form method="get" action="">
+<label>QD_ID <input type="text" name="QD_ID"/></label><br>
+
+<label>QUO_ID <select name="QUO_ID">
+<?php foreach($QUOTATION as $Q) {echo "<option value = $Q->QUO_ID>
+    $Q->QUO_ID</option>";}
+    ?>
+</select></label><br>
+
+<label>ProductID <select name="PRODSTOCK_ID">
+<?php foreach($productstockList as $PS){
+        echo "<option value = $PS->psid>
+        $PS->name $S->color</option>";}?>
+</select></label><br>
+
+<label>QD_QTY<input type="text" name="QD_QTY"/></label><br>
+<label>QD_ColorQTY<input type="text" name="QD_ColorQTY"/></label><br>
+<input type="hidden"name="controller"value="detailquotation"/>
+<button type= "submit"name="action"value="index">back</button>
+<button type= "submit"name="action"value="addQuotationDetail">Save</button>
+
+</form>
