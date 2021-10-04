636fd389a0b7270e8831e034bd23d4221b10abea
zinxia <tananan939@gmail.com>
2021-10-04 23:37:53 +0700

    update

--- views/quotation/deleteConfirm.php
+++ views/quotation/deleteConfirm.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+<center>
+<head>
+<style>
+body {
+              background-color: #FFCDD2;
+              color: #eb8c4a;
+              font-family:FC Friday;
+              text-align: center;
+               }
+</style>
+</head>
+<body>
+
 <?php echo "<br> ยืนยันจะลบใบเสนอราคา? <br>
             <br> $quotation->QUO_ID $quotation->C_Name <br>"; ?>
 
@@ -8,5 +23,8 @@
     <button type="submit" name="action" value="index">back</button>
     <button type="submit" name="action" value="delete">delete</button>
     
-
 </form>
+
+</center>
+</body>
+</html>
