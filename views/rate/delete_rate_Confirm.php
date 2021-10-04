70189bf2d1ac6b1470f3a9347c48936d92851354
asdofa <91516747+asdofa@users.noreply.github.com>
2021-10-05 00:04:12 +0700

    Update delete_rate_Confirm.php

--- views/rate/delete_rate_Confirm.php
+++ views/rate/delete_rate_Confirm.php
@@ -1,10 +1,10 @@
 <?php echo "<br> คุณยืนยันการกระทำนี้หรือไม่? <br>
-            <br> $RATE->RATE_ID $rate->RATE_ID <br>"; ?>
+            <br> $rate->RATE_ID <br>"; ?>
 
 <form method="get" action="">
 
     <input type="hidden" name="controller" value="rate" />
-    <input type="hidden" name="RATE_ID" value="<?php echo $RATE->RATE_ID; ?>" />
+    <input type="hidden" name="RATE_ID" value="<?php echo $rate->RATE_ID; ?>" />
     <button type="submit" name="action" value="index">back</button>
     <button type="submit" name="action" value="delete">delete</button>
     
