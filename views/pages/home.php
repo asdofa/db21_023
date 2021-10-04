bc6fdcb9830e2fd143505c2176acaec9437e8afc
zinxia <tananan939@gmail.com>
2021-10-04 22:51:03 +0700

    update

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,10 +1,20 @@
 <html>
 <center>
+    <style>
+        body {
+              background-color: #FFCDD2;
+              color: #eb8c4a;
+              font-family:FC Friday;
+              font-size: 40px;
+              text-align: center;
+               }
+    </style>
     <body>
-        <p>Welcome to our homepage</p>
+    <p>Welcome to Premium Shop</p>
         <br>
         <img src="https://i2.wp.com/traveler.gg/wp-content/uploads/kokomi-banner.png?fit=1024%2C765&ssl=1" width="1024" height="765" ><br><br>
         <br><br><br>
     </body>
+
 </center>
 </html>
