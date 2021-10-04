de6f5f4fcafdbeadb6cff85c25b34ecdef89d094
zinxia <tananan939@gmail.com>
2021-10-04 16:54:22 +0700

    update

--- controllers/connection_close.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $severname = "localhost";
-    $username = "db21_023";
-    $password ="db21_023";
-    $dbname = "db21_023";
-
-    $conn = mysqli_connect($severname,$username,$password);
-    $conn->close();
-?>
