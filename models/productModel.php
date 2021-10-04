efbb7cb0f077d3ffa70221ec1b40be9bf797ca45
NapatsornJR <91517820+NapatsornJR@users.noreply.github.com>
2021-10-04 22:47:11 +0700

    Update productModel.php

--- models/productModel.php
+++ models/productModel.php
@@ -3,16 +3,14 @@
     {
         public $PROD_ID;
         public $PROD_Name;
-        public $PROD_QTY;
         public $PROD_Category;
         public $PROD_Type;
         public $PROD_Minimum;
         public $PROD_Detail;
-        public function __construct($PROD_ID,$PROD_QTY,$PROD_Name,$PROD_Category,$PROD_Type,$PROD_Minimum,$PROD_Detail)
+        public function __construct($PROD_ID,$PROD_Name,$PROD_Category,$PROD_Type,$PROD_Minimum,$PROD_Detail)
         {
             $this->PROD_ID = $PROD_ID;
             $this->PROD_Name = $PROD_Name;
-            $this->$PROD_QTY = $PROD_QTY;
             $this->PROD_Category = $PROD_Category;
             $this->PROD_Type = $PROD_Type;
             $this->PROD_Minimum = $PROD_Minimum;
@@ -28,12 +26,11 @@
             {
                 $PROD_ID=$my_row[PROD_ID];
                 $PROD_Name=$my_row[PROD_Name];
-                $PROD_QTY = $my_row[PROD_QTY];
                 $PROD_Category=$my_row[PROD_Category];
                 $PROD_Type=$my_row[PROD_Type];
                 $PROD_Minimum=$my_row[PROD_Minimum];
                 $PROD_Detail=$my_row[PROD_Detail];
-                $productList[]=new Product($PROD_ID,$PROD_QTY,$PROD_Name,$PROD_Category,$PROD_Type,$PROD_Minimum,$PROD_Detail);
+                $productList[]=new Product($PROD_ID,$PROD_Name,$PROD_Category,$PROD_Type,$PROD_Minimum,$PROD_Detail);
             }
             require("connection_close.php");
 
